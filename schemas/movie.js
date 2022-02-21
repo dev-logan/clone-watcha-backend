92a91eaa3b4d2ae3394a601368ea4dad310cbf66
fancyers <kjng5901@gmail.com>
2022-02-21 23:44:51 +0900

    movieSchema 내용 추가

--- schemas/movie.js
+++ schemas/movie.js
@@ -1,5 +1,55 @@
 const mongoose = require('mongoose')
 
-const movieSchema = new mongoose.Schema({})
+const movieSchema = new mongoose.Schema({
+    movieId: {
+        type: Number,
+    },
+    title: {
+        type: String,
+    },
+    posterUrl: {
+        type: String,
+    },
+    year: {
+        type: Number,
+    },
+    genre: {
+        type: String,
+    },
+    country: {
+        type: String,
+    },
+    originalTitle: {
+        type: String,
+    },
+    duration: {
+        type: String,
+    },
+    age: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    galleryUrls: {
+        type: [String],
+    },
+    videoUrls: [
+        {
+            title: String,
+            url: String,
+        },
+    ],
+    credits: [
+        {
+            image: String,
+            name: String,
+            role: String,
+        },
+    ],
+    category: {
+        type: [String],
+    },
+})
 
 module.exports = mongoose.model('Movie', movieSchema, 'movies') //  아직 데이터 없지만 movies라는 collection에 데이터가 있다고 가정
